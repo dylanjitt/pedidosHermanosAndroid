27af507e1d8c80c49855a94e624a848c3ac0df87
Jose Samuel Carrasco Encinas <jsce2021@gmail.com>
2022-12-03 16:48:54 -0400

    Making my first commit

---
+++ app/src/main/java/com/grupof/aaft/Direccion_Pago.kt
@@ -0,0 +1,11 @@
+package com.grupof.aaft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Direccion_Pago : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_direccion_pago)
+    }
+}
