a67280c9f1e6a4c156afcb2215961380ea3e9bec
LucianoVargas <lutyvr02@gmail.com>
2022-12-11 20:13:37 -0400

    added some new activities

---
+++ app/src/main/java/com/grupof/aaft/Calificanos.kt
@@ -0,0 +1,11 @@
+package com.grupof.aaft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Calificanos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calificanos)
+    }
+}
